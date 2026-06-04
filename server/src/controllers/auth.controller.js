99153651f839662ae72db7690c6bf801b698ae09
HarshCodeCraft21 <harshj2103@gmail.com>
2026-06-04 16:30:51 +0530

    update cookie

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -108,11 +108,13 @@ const logoutUser = asyncHandler(async (req, res) => {
         }
     );
 
-    const options = {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production"
+    const options =  {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     }
-
+    
     return res
         .status(200)
         .clearCookie("accessToken", options)
