4bd571c71dbfe22378c890202f34dfe6fc3d7df8
HarshCodeCraft21 <harshj2103@gmail.com>
2026-06-05 14:00:27 +0530

    blank commit

--- server/src/routes/company.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { companyProfile } from "../controllers/company.controller.js";
-
-const companyRouter = express.Router();
-
-companyRouter.route("/companyprofile").post(companyProfile);
-
-export {companyRouter};
